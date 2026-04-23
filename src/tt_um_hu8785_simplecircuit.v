94e7c0430cc1901fa60e800a441c838f1d097302
hu8785 <patilaku96@gmail.com>
2026-04-23 15:43:53 -0400

    Update and rename tt_um_akaksha_hu8785_simplecircuit.v to tt_um_hu8785_simplecircuit.v

---
+++ src/tt_um_hu8785_simplecircuit.v
@@ -0,0 +1,42 @@
+`default_nettype none
+
+module tt_um_hu8785_simple_circuit (
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
+);
+
+    wire A = ui_in[0];
+    wire B = ui_in[1];
+    wire C = ui_in[2];
+
+    wire e;
+    wire x;
+    wire y;
+
+    assign e = A & B;
+    assign y = ~C;
+    assign x = e | y;
+
+    assign uo_out[0] = x;
+    assign uo_out[1] = y;
+    assign uo_out[2] = 1'b0;
+    assign uo_out[3] = 1'b0;
+    assign uo_out[4] = 1'b0;
+    assign uo_out[5] = 1'b0;
+    assign uo_out[6] = 1'b0;
+    assign uo_out[7] = 1'b0;
+
+    assign uio_out = 8'b00000000;
+    assign uio_oe  = 8'b00000000;
+
+    wire _unused = &{ena, clk, rst_n, ui_in[7:3], uio_in, 1'b0};
+
+endmodule
+
+`default_nettype wire
