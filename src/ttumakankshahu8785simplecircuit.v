49da121c19238155a58c0b9d3af7c74805f40ed6
hu8785 <patilaku96@gmail.com>
2026-04-22 22:11:49 -0400

    Update and rename ttumhu8785simplecircuit.v to ttumakankshahu8785simplecircuit.v

--- src/ttumakankshahu8785simplecircuit.v
+++ src/ttumakankshahu8785simplecircuit.v
@@ -1,11 +1,11 @@
 `default_nettype none
-module ttumhu8785simplecircuit (
+module ttumakankshahu8785simplecircuit (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       ena,      // always 1 when the design is powered
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
